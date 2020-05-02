05251d3c108deac3731e8be6bc7c3a46e5466d87
Sai Teja Malladi <saiteja.malladi@gmail.com>
2020-05-02 20:09:49 +0530

    Reindented Layout File

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 import Aux from '../../hoc/Auxiliary';
 const layout = (props) => (
-    <Aux>
-        <div>Toolbar, SideDrawer, Backdrop...</div>
-        <div>
-            {props.children}
-        </div>
-    </Aux>
+	<Aux>
+		<div>Toolbar, SideDrawer, Backdrop...</div>
+		<div>
+			{props.children}
+		</div>
+	</Aux>
 );
 
 export default layout;
