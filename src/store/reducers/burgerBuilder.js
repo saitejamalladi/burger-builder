0ebd6759e45b9e0f815e5351d50b28332cd13717
Sai Teja Malladi <saiteja.malladi@gmail.com>
2020-05-08 02:01:05 +0530

    reducers restructured

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -1,5 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
-
+import { updatedObj } from '../Utility';
 const INGREDIENT_PRICES = {
 	meat: 1.30,
 	cheese: 0.7,
@@ -12,39 +12,51 @@ const initialState = {
 	error: false
 };
 
-const burgerBuilder = (state = initialState, action) => {
-	switch (action.type) {
-		case actionTypes.INIT_INGREDIENTS:
-			return {
-				...state,
-				ingredients: action.ingredients,
-				price: 4
-			};
-		case actionTypes.FETCH_INGREDIENTS_FAILED:
-			return {
-				...state,
-				error: true
-			};
-		case actionTypes.ADD_INGREDIENTS:
-			return {
-				...state,
-				ingredients: {
-					...state.ingredients,
-					[action.ingredientName]: state.ingredients[action.ingredientName] + 1
-				},
-				price: state.price + INGREDIENT_PRICES[action.ingredientName]
-			};		case actionTypes.REMOVE_INGREDIENTS:
-			return {
-				...state,
-				ingredients: {
-					...state.ingredients,
-						[action.ingredientName]: state.ingredients[action.ingredientName] - 1
-				},
-				price: state.price - INGREDIENT_PRICES[action.ingredientName]
-			};
-		default:
-			return state;
+const addIngredient = ( state, action ) => {
+	const updatedIngredient = { [action.ingredientName]: state.ingredients[action.ingredientName] + 1 }
+	const updatedIngredients = updatedObj( state.ingredients, updatedIngredient );
+	const updatedState = {
+		ingredients: updatedIngredients,
+		totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
+	};
+	return updatedObj( state, updatedState );
+};
+
+const removeIngredient = (state, action) => {
+	const updatedIng = { [action.ingredientName]: state.ingredients[action.ingredientName] - 1 }
+	const updatedIngs = updatedObj( state.ingredients, updatedIng );
+	const updatedSt = {
+		ingredients: updatedIngs,
+		totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
+	};
+	return updatedObj( state, updatedSt );
+};
+
+const setIngredients = (state, action) => {
+	return updatedObj( state, {
+		ingredients: {
+			salad: action.ingredients.salad,
+			bacon: action.ingredients.bacon,
+			cheese: action.ingredients.cheese,
+			meat: action.ingredients.meat
+		},
+		totalPrice: 4,
+		error: false
+	} );
+};
+
+const fetchIngredientsFailed = (state, action) => {
+	return updatedObj( state, { error: true } );
+};
+
+const reducer = ( state = initialState, action ) => {
+	switch ( action.type ) {
+		case actionTypes.ADD_INGREDIENTS: return addIngredient( state, action );
+		case actionTypes.REMOVE_INGREDIENTS: return removeIngredient(state, action);
+		case actionTypes.INIT_INGREDIENTS: return setIngredients(state, action);
+		case actionTypes.FETCH_INGREDIENTS_FAILED: return fetchIngredientsFailed(state, action);
+		default: return state;
 	}
 };
 
-export default burgerBuilder;
+export default reducer();
--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -1,51 +1,41 @@
 import * as actionTypes from '../actions/actionTypes';
+import {updatedObj} from "../Utility";
 
 const initialState = {
 	orders: [],
-	error: null,
 	loading: false,
 	purchased: false,
 };
 
-const order = (state = initialState, action) => {
+const purchaseBurgerStart = (state, action) => {
+	return updatedObj(...state, {loading: true, purchased: false});
+};
+const purchaseBurgerSuccess = (state, action) => {
+	return updatedObj(...state, {loading: false, purchased: true});
+};
+const purchaseBurgerFailed = (state, action) => {
+	return updatedObj(...state, {loading: false});
+};
+const fetchOrdersStart = (state, action) => {
+	return updatedObj(...state, {loading: true});
+};
+const fetchOrdersSuccess = (state, action) => {
+	return updatedObj(...state, {loading: false, orders: action.orders});
+};
+const fetchOrdersFailed = (state, action) => {
+	return updatedObj(...state, {loading: false});
+};
+const reducer = (state = initialState, action) => {
 	switch (action.type) {
-		case actionTypes.PURCHASE_BURGER_START:
-			return {
-				...state,
-				loading: true,
-				purchased: false
-			};
-		case actionTypes.PURCHASE_BURGER_SUCCESS:
-			return {
-				...state,
-				loading: false,
-				purchased: true
-			};
-		case actionTypes.PURCHASE_BURGER_FAILED:
-			return {
-				...state,
-				loading: false
-			};
-		case actionTypes.FETCH_ORDERS_START:
-			return {
-				...state,
-				loading: true
-			};
-		case actionTypes.FETCH_ORDERS_SUCCESS:
-			return {
-				...state,
-				loading: false,
-				orders: action.orders
-			};
-		case actionTypes.FETCH_ORDERS_FAILED:
-			return {
-				...state,
-				loading: false,
-				error: action.error
-			};
+		case actionTypes.PURCHASE_BURGER_START: return purchaseBurgerStart(state, action);
+		case actionTypes.PURCHASE_BURGER_SUCCESS: return purchaseBurgerSuccess(state, action);
+		case actionTypes.PURCHASE_BURGER_FAILED: return purchaseBurgerFailed(state, action);
+		case actionTypes.FETCH_ORDERS_START: return fetchOrdersStart(state, action);
+		case actionTypes.FETCH_ORDERS_SUCCESS: return fetchOrdersSuccess(state, action);
+		case actionTypes.FETCH_ORDERS_FAILED: return fetchOrdersFailed(state, action);
 		default:
 			return state;
 	}
 };
 
-export default order;
+export default reducer;
